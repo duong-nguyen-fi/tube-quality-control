e298bcd561627f902d820fe6cb94d5332d035602
Xia <e1701269@edu.vamk.fi>
2018-11-15 19:55:35 +0200

    add xml writer

--- TubeQualityControl/Entity/Part.cs
+++ TubeQualityControl/Entity/Part.cs
@@ -6,20 +6,27 @@ using System.Threading.Tasks;
 
 namespace TubeQualityControl.Entity
 {
-    
     class Part
     {
         public string Name { get; set; }
 
-        public int SuggestPoints { get; set; }
+        public int Suggest_points { get; set; }
 
         public List<MeasurePoint> MeasurePoints { get; set; }
 
-        public Part(string name, int suggestPoints)
+        public Part(string name, int suggest_points, List<MeasurePoint> MeasurePoints)
         {
             Name = name;
-            SuggestPoints = suggestPoints;
+            Suggest_points = suggest_points;
             MeasurePoints = new List<MeasurePoint>();
         }
+
+        public Part() { }
+
+     
+      /*  public override String ToString() {
+            return Name + Suggest_points+ String.Join(",",MeasurePoints);
+           
+        }*/
     }
 }
---
+++ TubeQualityControl/XmlHandler/XmlWriter.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TubeQualityControl.Entity
+{
+    class XmlWriter
+    {
+
+        public static string WriteXML(Part part)
+        {
+            string filePathName = @"U:\C#\TubeQualityControl.Entity\tube.txt";
+            //Here we use the XmlTextWriter to open a new XML file
+            // FileStream filestream = new FileStream(filePathName, FileMode.Append);
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(filePathName, System.Text.Encoding.UTF8);
+
+
+            xmlTextWriter.Formatting = Formatting.Indented;
+            //Here we write XML declaration
+            xmlTextWriter.WriteStartDocument();
+            //Here we write the root elemnt
+            xmlTextWriter.WriteStartElement("root");
+            //Here we end the root element
+            xmlTextWriter.WriteEndElement();
+            //Here we end the document element
+            xmlTextWriter.WriteEndDocument();
+            //Here we flush and close the stream
+
+            //xmlTextWriter.Flush();
+            xmlTextWriter.Close();
+            FileInfo fileInfo = new FileInfo(filePathName);
+
+            //Here we return the file path and its length. 
+            return Path.GetFullPath(filePathName) + " was written succefully. File size: " + fileInfo.Length + ".";
+        }
+
+
+        //This method returns file information
+        public string GetFileInfo(string filePathName)
+        {
+            //Here we create a FileInfo object
+            FileInfo fileInfo = new FileInfo(filePathName);
+
+
+            //Here we return the file path and its length. 
+            return Path.GetFullPath(filePathName) + " was written succefully. File size: " + fileInfo.Length + ".";
+        }
+
+
+        //This method appends new elements to the XML tree.
+        public static string AppendElement(Part part)
+        {
+            string filePathName = @"U:\C#\TubeQualityControl.Entity\tube.txt";
+            //Reading and adding new data
+            XmlTextReader xmlTextReader = new XmlTextReader(filePathName);
+            //Does not return any whitespace node
+            xmlTextReader.WhitespaceHandling = WhitespaceHandling.None;
+            //Here we load the file into memory
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(xmlTextReader);
+            xmlTextReader.Close();
+            XmlElement newTubeElement = xmlDocument.CreateElement("data");
+            newTubeElement.SetAttribute("type",part.Name);
+           // XmlElement newDataElement = xmlDocument.CreateElement("point");
+            //newTubeElement.AppendChild(newDataElement);
+
+            /*  int counter;
+              for (counter = 0; counter <part.MeasurePoints.Count; counter++)
+              {
+                  XmlElement newXElement = xmlDocument.CreateElement("X");
+                  newXElement.InnerText = part.MeasurePoints[counter].X.ToString();
+                  newDataElement.AppendChild(newXElement);
+                  XmlElement newYElement = xmlDocument.CreateElement("Y");
+                  newYElement.InnerText = part.MeasurePoints[counter].Y.ToString();
+                  newDataElement.AppendChild(newYElement);
+                  XmlElement newZElement = xmlDocument.CreateElement("Z");
+                  newZElement.InnerText = part.MeasurePoints[counter].Z.ToString();
+                  newDataElement.AppendChild(newZElement);
+              }*/
+
+            XmlNode rootNode = xmlDocument.DocumentElement;
+            // xmlDocument.GetElementById("employees").AppendChild(newEmpElement);
+
+            //Here we would add the new child to the end of the root node
+            rootNode.AppendChild(newTubeElement);
+            xmlDocument.Save(filePathName);
+            return "Add child element successfully";
+          // return GetFileInfo(filePathName);
+        }
+
+
+
+        public static string AppendElement(MeasurePoint p)
+        {
+            string filePathName = @"U:\C#\TubeQualityControl.Entity\tube.txt";
+            //Reading and adding new data
+            XmlTextReader xmlTextReader = new XmlTextReader(filePathName);
+            //Does not return any whitespace node
+            xmlTextReader.WhitespaceHandling = WhitespaceHandling.None;
+            //Here we load the file into memory
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(xmlTextReader);
+            xmlTextReader.Close();
+            XmlElement newDataElement = xmlDocument.CreateElement("point");
+            XmlElement newXElement = xmlDocument.CreateElement("X");
+            newXElement.InnerText = p.X.ToString();
+            newDataElement.AppendChild(newXElement);
+            XmlElement newYElement = xmlDocument.CreateElement("Y");
+            newYElement.InnerText = p.Y.ToString();
+            newDataElement.AppendChild(newYElement);
+            XmlElement newZElement = xmlDocument.CreateElement("Z");
+            newZElement.InnerText = p.Z.ToString();
+            newDataElement.AppendChild(newZElement);
+             
+
+            XmlNode rootNode = xmlDocument.DocumentElement;
+            // xmlDocument.GetElementById("employees").AppendChild(newEmpElement);
+
+            //Here we would add the new child to the end of the root node
+            rootNode.AppendChild(newDataElement);
+            xmlDocument.Save(filePathName);
+            return "Add child eöement successfully";
+            // return GetFileInfo(filePathName);
+        }
+
+        private static void AddChildren(XmlNode xmlNode, int level)
+        {
+            XmlNode childXmlNode;
+            //Here we speciy padding; th enumber of spaces based on the level in the XML tree
+            string pad = new string(' ', level * 2);
+
+            Console.WriteLine(pad + xmlNode.Name + "(" + xmlNode.NodeType.ToString() + ": " + xmlNode.Value + ")");
+            //Here we extract possible attributes
+            if (xmlNode.NodeType == XmlNodeType.Element)
+            {
+
+                XmlNamedNodeMap mapAttributes = xmlNode.Attributes;
+                for (int i = 0; i < mapAttributes.Count; i++)
+                {
+                    Console.WriteLine(pad + " " + mapAttributes.Item(i).Name + "=" + mapAttributes.Item(i).Value);
+                }
+
+            }
+            //Here we call recursively o all children of the current node
+            if (xmlNode.HasChildNodes)
+            {
+                childXmlNode = xmlNode.FirstChild;
+                while (childXmlNode != null)
+                {
+                    AddChildren(childXmlNode, level + 1);
+                    childXmlNode = childXmlNode.NextSibling;
+                }
+            }
+        }
+    }
+}
