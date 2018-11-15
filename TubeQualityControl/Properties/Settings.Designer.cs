baeff502e4dd9b8d2394cf9a73fd7cb927fec425
e1500948 <tynshjt@gmail.com>
2018-11-15 16:51:19 +0200

    Invoke Octave script

--- TubeQualityControl/Properties/Settings.Designer.cs
+++ TubeQualityControl/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TubeQualityControl.Properties
-{
-
-
+namespace TubeQualityControl.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("D:\\Software\\Octave-4.4.1\\bin\\octave-cli.exe")]
+        public string Octave_Path {
+            get {
+                return ((string)(this["Octave_Path"]));
+            }
+            set {
+                this["Octave_Path"] = value;
             }
         }
     }
