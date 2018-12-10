85b259018a22542ce1e60dbed0b5fa276c5bc437
Nguyen, Duong <e1500948@edu.vamk.fi>
2018-12-10 21:11:14 +0200

    Listen to db and event to add point to part

---
+++ TubeQualityControl/Entity/NewDataEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TubeQualityControl.Entity
+{
+    
+        public class NewDataEventArgs : EventArgs
+        {
+            private int id;
+            public NewDataEventArgs(int _id)
+            {
+                id = _id;
+            }
+            public int GetId()
+            {
+                return id;
+            }
+        }
+    
+}
---
+++ TubeQualityControl_Test/PollData/Poll_New_Data.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TubeQualityControl.DbHandler;
+using TubeQualityControl.Entity;
+using System.Diagnostics;
+
+namespace TubeQualityControl_Test.PollData
+{
+    [TestClass]
+    public class Poll_New_Data
+    {
+        [TestMethod]
+        public void StartDbService_Get_NewData()
+        {
+            Part _Part = new Part();
+            bool newPoint = false;
+
+            DbService service = new DbService();
+            service.Start();
+            service.NewDataReceived += (sender, e) =>
+            {
+                
+                newPoint = _Part.AddPoint(new MeasurePoint(GetRandomNumber(29.12,120.12), GetRandomNumber(29.12, 120.12), GetRandomNumber(29.12, 120.12)));
+
+                
+            };
+
+            while (!newPoint) { }
+
+            Debug.WriteLine(_Part);
+
+            Assert.AreNotEqual(0, _Part.PointCount);
+            
+        }
+
+        public double GetRandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
